0a5d41fe32e8c2fa024f8cc00eefa4678356e7f9
Samy Gejzenblozen <samy.gejz@gmail.com>
2016-05-07 19:53:11 +0200

    Added Smoke. On standby for now

---
+++ cerbereapp/static/js/app_effects.js
@@ -0,0 +1,13 @@
+function sayHello(){
+  alert("Hello world!!!")
+}
+
+ $('#alertDanger') {
+   e.preventDefault();
+   $.smkAlert({
+     text: 'Alert type "danger" time 10 seconds',
+     type: 'danger',
+     position:'top-center',
+     time: 10
+   });
+ });
